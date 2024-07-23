95f3f94749fe4beeb7467f883b51218b52660d21
Jake Darby <designdarby@gmail.com>
2024-07-23 17:50:47 +0100

    adding radix theme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
+import { Button, Flex, Heading, Card } from "@radix-ui/themes";
 
 export default function Header() {
   return (
     <>
-      <h1>Header</h1>
-      <div id="navBox">
-        <nav className="navBar">
-          <Link to="/">Home</Link>
-          <Link to="/api">Api</Link>
-          <Link to="/dashboard">Dashboard</Link>
-        </nav>
-      </div>
+      <Card>
+        <Flex direction="row" align="center" justify="between" wrap="wrap">
+          <Heading>Apex.js charts - Data visualisation</Heading>
+          <Flex gap="2">
+            <Button variant="outline">
+              <Link to="/">Home</Link>
+            </Button>
+            <Button variant="outline">
+              <Link to="/api">Api</Link>
+            </Button>
+            <Button variant="outline">
+              <Link to="/dashboard">Dashboard</Link>
+            </Button>
+          </Flex>
+        </Flex>
+      </Card>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+//importing theme
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </BrowserRouter>
   </React.StrictMode>
 );
