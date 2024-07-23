bda191b9f8c1ebbee39d3bedfcf34f36ce326188
Theo Reeves <theo_25@icloud.com>
2024-07-23 14:11:52 +0100

    new api key

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import MyComponent from "../components/ReactBarChart";
 export default function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>Home Page </h1>
       <MyComponent />
       <LineChart />
       <Donut />
