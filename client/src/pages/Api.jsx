9d5abeea9a69937a0d4ed41df67f7432d077ba87
Dorkydubs <rsgacre@gmail.com>
2024-07-25 11:55:02 +0100

    drawing from db to create chart data

--- client/src/pages/Api.jsx
+++ client/src/pages/Api.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
-
+import CandleStick from "../components/CandleStick";
 export default function ApiPage() {
   return (
     <>
       <h1>Apple Stock Data</h1>
-
+      <CandleStick />
     </>
   );
 }
